458a3755a8d205658c246d40d9f0f59eca36672b
confianzaseguridad772-max <confianzaseguridad772@gmail.com>
2026-04-08 13:07:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==========================================
 // 1. CONFIGURACIÓN
 // ==========================================
-const URL_APPS_SCRIPT = "https://script.google.com/macros/s/AKfycbxCNO2EvNomfIOmOBNmzNbRziRXCVPt3r9IXlwrO9hJiisUtiw0hkRSSHxCXwHprflrsQ/exec"; 
+const URL_APPS_SCRIPT = "https://script.google.com/macros/s/AKfycbyhM22q0n6x0HS2CHAVcUi81_khMjj7LSHv2GzxBGyqzj1761A-eRZPWQnyEh0n6r1P_Q/exec"; 
 let padronData = [];
 let metodoRegistro = "Manual"; 
 const fields = ['nombres', 'apPaterno', 'apMaterno', 'fechaNac', 'sexo', 'celular', 'direccion'];
